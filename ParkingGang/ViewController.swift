5c43828bc54a2681896db17f86b5e7802442705c
seguev <peretzsegev@gmail.com>
2024-01-22 20:55:14 +0200

    Initial Commit

---
+++ ParkingGang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkingGang
+//
+//  Created by segev perets on 22/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ParkingGangUITests/ParkingGangUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ParkingGangUITestsLaunchTests.swift
+//  ParkingGangUITests
+//
+//  Created by segev perets on 22/01/2024.
+//
+
+import XCTest
+
+final class ParkingGangUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
